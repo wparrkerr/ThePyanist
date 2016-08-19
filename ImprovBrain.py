e397106788c65b2f795ea2f93008f1b7c1b67192
Will Parker <wparker1106@gmail.com>
2016-08-18 23:42:40 -0400

    added brain

---
+++ ImprovBrain.py
@@ -0,0 +1,9 @@
+class Brain():
+	def __init__(self, motive):
+		self.baseMotive = motive
+
+	def setBaseMotive(self, newMotive):
+		self.baseMotive = newMotive
+
+	def getBar(self):
+		return (["["C", "F", "G", "C"])
--- pyanist.py
+++ pyanist.py
@@ -1,2 +1,33 @@
 #Actual project file
 #This is the main file, it handles I/O, and uses the brain for logic
+
+# from mingus.midi import fluidsynth
+# from mingus.containers import Note
+# from mingus.containers import NoteContainer
+# import mingus.core.chords as chords
+# import mingus.core.scales as scales
+# from time import sleep
+from ImprovBrain import Brain
+
+global brain
+
+def setup():
+	global brain
+	brain = Brain()
+
+def playNotesInList(listOfNotes):
+	"""
+	Plays musical notes from a list using mingus/fluidsynth
+
+	Input form example: []
+	"""
+
+def main():
+	global brain
+	while True:
+		nextNoteSequence = brain.getBar()
+
+
+if __name__ == "__main__":
+	setup()
+	main()
